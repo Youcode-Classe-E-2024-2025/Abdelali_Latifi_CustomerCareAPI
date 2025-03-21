b6c88915dfba56c9e938ec553ab382adee9ecb3c
latifi777 <abdelalilatifi3@gmail.com>
2025-03-21 10:12:58 +0000

    created Authservice with implimebtation

---
+++ app/Services/AuthService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Auth;
+
+class AuthService
+{
+    public function register(array $data)
+    {
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'role' => $data['role'] ?? 'client',
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return ['user' => $user, 'token' => $token];
+    }
+
+    public function login(array $credentials)
+    {
+        $user = User::where('email', $credentials['email'])->first();
+
+        if (!$user || !Hash::check($credentials['password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['Les informations d’identification sont incorrectes.'],
+            ]);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return ['user' => $user, 'token' => $token];
+    }
+
+    public function logout()
+    {
+        Auth::user()->tokens()->delete();
+        return ['message' => 'Déconnexion réussie'];
+    }
+
+    public function getUser()
+    {
+        return Auth::user();
+    }
+}

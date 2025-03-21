f2cd44fc0f85bae56d130a557acef8e0f4b0ad08
latifi777 <abdelalilatifi3@gmail.com>
2025-03-21 15:01:44 +0000

    correcte ticket controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TicketsController;
 
 
 /*
@@ -26,4 +27,18 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'getUser']);
+
+    Route::get('/tickets', [TicketsController::class, 'index']); 
+    Route::get('/my-tickets', [TicketsController::class, 'userTickets']);
+    Route::get('/tickets/{id}', [TicketsController::class, 'show']); 
+    Route::post('/tickets', [TicketsController::class, 'store']); 
+    Route::put('/tickets/{id}', [TicketsController::class, 'update']); 
+    Route::delete('/tickets/{id}', [TicketsController::class, 'destroy']); 
+
+    Route::put('/tickets/{id}/start', [TicketsController::class, 'start']); 
+    Route::put('/tickets/{id}/close', [TicketsController::class, 'close']); 
+
+    Route::get('/tickets/open', [TicketsController::class, 'openTickets']); 
+    Route::get('/tickets/in-progress', [TicketsController::class, 'inProgressTickets']); 
+    Route::get('/tickets/closed', [TicketsController::class, 'closedTickets']); 
 });
